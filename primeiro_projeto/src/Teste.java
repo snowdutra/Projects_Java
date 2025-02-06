de51c8896fbdabb6bf949ed3ead0994a639aa6df
Gustavo Dutra <159259903+snowdutra@users.noreply.github.com>
2025-02-06 10:18:44 -0300

    Add files via upload

---
+++ primeiro_projeto/src/Teste.java
@@ -0,0 +1,8 @@
+public class Teste {
+    public static void main(String[] args) {
+        System.out.println("Teste");
+        System.out.println("Java");
+        System.out.print("É mágica!");
+        System.out.println( "Não é não! É Java!");
+    }
+}
