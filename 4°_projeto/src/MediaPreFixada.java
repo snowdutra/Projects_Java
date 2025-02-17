e07197869b7b519a06c66073d486bffa0bf4211d
Gustavo Dutra <gusdutratelles@icloud.com>
2025-02-17 12:16:44 -0300

    Média Pré Fixada

--- 4°_projeto/src/MediaPreFixada.java
+++ 4°_projeto/src/MediaPreFixada.java
@@ -1,21 +1,20 @@
- public class MediaPreFixada   {
-    /**
-     * Classe que calcula a m dia prefixada
-     */
+public class MediaPreFixada   {
+
     public static void main(String[] args) {
         int[] x = {10, 5, 12, 3, 2};
         double[] media = new double[x.length];
 
-        /**
-         * O loop percorre o vetor de n meros e calcula a m dia prefixada
-         * para cada um deles
-         * @param i   o   ndice do vetor
-         */
         for(int i = 0; i < x.length; i++) {
             int soma = 0;
             for(int j = 0; j <= i; j++) {
                 soma += x[j];
             }
-            media[i] = (double)soma / (i+1);
+            media[i] = soma / (i+1);
         }
- }
+
+        for(int i = 0; i < media.length; i++) {
+            System.out.println(media[i] + " ");
+        }
+    }
+}
+
